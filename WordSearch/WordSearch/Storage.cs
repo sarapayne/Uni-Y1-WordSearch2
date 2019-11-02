521365c39dfabf4c5a6000af636dd20e18e34125
sarapayne <56539249+sarapayne@users.noreply.github.com>
2019-11-02 15:45:26 +0000

    added storage class

---
+++ WordSearch/WordSearch/Storage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearch
+{
+    class Storage
+    {
+        private string[] files;
+        private string[] fileContents;
+        private string[] defaultGame; //default game list, same format as the loaded games files
+
+        public Storage()
+        {
+            files = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.wrd");
+            defaultGame = new string[] { "9,5,2", "algorithm,0,1,right", "virus,5,3,left" };
+        }
+
+        public string[] Files
+        {
+            get { return this.files; }
+            set { this.files = value; }
+        }
+
+        public string[] FileContents
+        {
+            get { return this.fileContents; }
+            set { this.fileContents = value; }
+        }
+
+        public void LoadFile(int filesIndex)
+        {
+            if (filesIndex == -1)
+            {
+                fileContents = defaultGame;
+            }
+            else
+            {
+                fileContents = File.ReadAllLines(files[filesIndex]);
+            }
+            Program.validation.GameFile(fileContents);
+        }
+    }
+}
