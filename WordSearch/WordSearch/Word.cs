c8d00b413d30a81eb220c61bf07e8605199987c6
sarapayne <56539249+sarapayne@users.noreply.github.com>
2019-11-02 15:30:27 +0000

    added word class

---
+++ WordSearch/WordSearch/Word.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearch
+{
+    class Word
+    {
+        private string name;
+        private bool found;
+
+        public Word()
+        {
+
+        }
+
+        public Word(string wordName, bool isfound)
+        {
+            this.name = wordName;
+            this.found = isfound;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public bool Found
+        {
+            get { return this.found; }
+            set { this.found = value; }
+        }
+    }
+}
