067fb9cd4db737300b5920f925a469abfaeab550
sarapayne <56539249+sarapayne@users.noreply.github.com>
2019-11-02 16:33:17 +0000

    added getters and setters to new GameFile class

---
+++ WordSearch/WordSearch/GameFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearch
+{
+    class GameFile
+    {
+        private string name;
+        private string[] fileContents;
+        private bool validated;
+
+        public string Name
+        {
+            get { return this.Name; }
+            set { this.Name = value; }
+        }
+
+        public string[] FileContents
+        {
+            get { return this.fileContents; }
+            set { this.fileContents = value; }
+        }
+
+        public bool Validated
+        {
+            get { return this.validated; }
+            set { this.validated = value; }
+        }
+    }
+}
--- WordSearch/WordSearch/WordSearch.cs
+++ WordSearch/WordSearch/WordSearch.cs
@@ -10,14 +10,19 @@ namespace WordSearch
         private Validation validation;
         private List<Word> wordsInCurrentGame;
         private Storage storage;
+        private int gameIndex;
 
+        ConsoleColor textColour;
+        Vector lastWrongStartIndex;
+        Vector lastWrongEndIndex;
+        
         public WordSearch()
         {
             validation = new Validation();
             storage = new Storage();
             InitialMenu();
-            PrintWordChoices();
-            InGameChoices();
+            //PrintWordChoices();
+            //InGameChoices();
         }
 
         private void InitialMenu()
@@ -32,7 +37,7 @@ namespace WordSearch
             string menuChoice = Console.ReadLine();
             menuChoice = validation.InitialMenu(menuChoice);
             LoadGameChoice(menuChoice);
-            wordSearch = new Board(userInput);
+            //wordSearch = new Board(userInput);
         }
 
         private void LoadGameChoice(string menuChoice)
@@ -43,18 +48,34 @@ namespace WordSearch
             }
             else if (menuChoice == "2")
             {
-                loadedGame = new List<string>();
-
                 gameIndex = FileChoicesMenu();
             }
             storage.LoadFile(gameIndex);
+
+            /*
             textColour = new ConsoleColor();
-            lastWrongStart = new Vector();
-            lastWrongEnd = new Vector();
+            lastWrongStartIndex = new Vector();
+            lastWrongEndIndex = new Vector();
             //cellObjects = new List<GameObject>[Program.validation.CellObjects.GetLength(0), Program.validation.CellObjects.GetLength(1)];
             FillEmptyCells();
             DisplayBoard();
+            */
+        }
+
+        private int FileChoicesMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Select a file to load");
+            for (int filesIndex = 0; filesIndex < storage.Files.Length; filesIndex++)
+            {
+                Console.WriteLine(filesIndex + 1 + ". " + storage.Files[filesIndex]);
+            }
+            Console.WriteLine("\n" +
+                "Enter a number from: 1 to " + storage.Files.Length + " inclusive");
+            int userChoice = validation.CheckIntInRange(Console.ReadLine(), 1, storage.Files.Length);
+            userChoice--; //subtract 1 to give the file index in the array of availible files
+            return userChoice;
         }
     }
 }
-}
+
