688264a8d6ac4da0e0be51c242fe752b59f4baca
sarapayne <56539249+sarapayne@users.noreply.github.com>
2019-11-02 15:29:05 +0000

    added Vectore class

---
+++ WordSearch/WordSearch/Vector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearch
+{
+    class Vector
+    {
+        private int row;
+        private int collum;
+
+        public Vector()
+        {
+
+        }
+
+        public Vector(int inputRow, int inputCollum)
+        {
+            this.row = inputRow;
+            this.collum = inputCollum;
+        }
+
+        public int Row
+        {
+            get { return this.row; }
+            set { this.row = value; }
+        }
+
+        public int Collum
+        {
+            get { return this.collum; }
+            set { this.collum = value; }
+        }
+    }
+}
