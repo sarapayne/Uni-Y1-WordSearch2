e350e1811da67b9f89aa917eb2bdead3ea7b7735
sarapayne <56539249+sarapayne@users.noreply.github.com>
2019-11-02 15:35:15 +0000

    added letter class

---
+++ WordSearch/WordSearch/Letter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordSearch
+{
+    class Letter
+    {
+
+        private bool wordStart;
+        private bool wordEnd;
+        private Vector position;
+        private string character;
+        private string word;
+        private string direction;
+        private ConsoleColor color;
+
+        public Letter()
+        {
+            color = ConsoleColor.White;
+        }
+
+        public Letter(ConsoleColor colour)
+        {
+            color = colour;
+        }
+
+        public Letter(bool isStart, bool isEnd, Vector pos, string character, string inWord, ConsoleColor colour)
+        {
+            wordStart = isStart;
+            wordEnd = isEnd;
+            position = pos;
+            letter = character;
+            word = inWord;
+            color = colour;
+        }
+
+        public string Direction
+        {
+            get { return this.direction; }
+            set { this.direction = value; }
+        }
+
+        public bool WordStart
+        {
+            get { return this.wordStart; }
+            set { this.wordStart = value; }
+        }
+
+        public bool WordEnd
+        {
+            get { return this.wordEnd; }
+            set { this.wordEnd = value; }
+        }
+
+        public Vector Positon
+        {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+
+        public string Character
+        {
+            get { return this.Character; }
+            set { this.Character = value; }
+        }
+
+        public string Word
+        {
+            get { return this.word; }
+            set { this.word = value; }
+        }
+
+        public ConsoleColor Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+    }
+}
